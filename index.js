29faf7b5713c5f30fbf9ce5ab399291cccbb8454
Milos89J <91075244+Milos89J@users.noreply.github.com>
2023-11-09 19:58:47 +0100

    prod and dev

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<p>Docker-node-funap</p>");
+  res.send("<p>Docker-node-funap.prod-no</p>");
 
 });
 
