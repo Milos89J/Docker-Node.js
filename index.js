563341b2aba04638e75d72f729d8ba32b69cbc7d
Milos89J <91075244+Milos89J@users.noreply.github.com>
2023-11-09 19:23:29 +0100

    .yml

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<p>Docker-node-funapp</p>");
+  res.send("<p>Docker-node-funap</p>");
 
 });
 
